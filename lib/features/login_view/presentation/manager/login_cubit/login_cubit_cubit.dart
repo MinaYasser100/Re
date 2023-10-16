b8cd4250c7964044c0b325932311bb92fff85447
MinaYasser100 <minayasser43@gmail.com>
2023-10-16 20:04:16 +0300

    do login method

--- lib/features/login_view/presentation/manager/login_cubit/login_cubit_cubit.dart
+++ lib/features/login_view/presentation/manager/login_cubit/login_cubit_cubit.dart
@@ -1,7 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 part 'login_cubit_state.dart';
 
 class LoginCubitCubit extends Cubit<LoginCubitState> {
   LoginCubitCubit() : super(LoginCubitInitial());
+
+  void loginUserInApp({required String email, required String password}) {
+    emit(LoginCubitLoginUserLoading());
+    try {
+      FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      emit(LoginCubitLoginUserSuccess());
+    } catch (e) {
+      emit(
+        LoginCubitLoginUserfailure(errorMessage: e.toString()),
+      );
+    }
+  }
 }
--- lib/features/login_view/presentation/manager/login_cubit/login_cubit_state.dart
+++ lib/features/login_view/presentation/manager/login_cubit/login_cubit_state.dart
@@ -4,3 +4,13 @@ part of 'login_cubit_cubit.dart';
 sealed class LoginCubitState {}
 
 final class LoginCubitInitial extends LoginCubitState {}
+
+final class LoginCubitLoginUserLoading extends LoginCubitState {}
+
+final class LoginCubitLoginUserSuccess extends LoginCubitState {}
+
+final class LoginCubitLoginUserfailure extends LoginCubitState {
+  final String errorMessage;
+
+  LoginCubitLoginUserfailure({required this.errorMessage});
+}
